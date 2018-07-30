cfca82f98c1c73a9191590a0379b1e632db0f2ea
khalid jamal <kh.jamal95@gmail.com>
2018-07-30 18:43:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ class BoardMember{
   approve(){return "You can do that!"}
   doCharity(){return "I like to help people."}
   releasePressStatement(){return "You will see great things from Scuber."}
-  sayHi(){return "No, I must disagree"}
+  sayHi(){return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`}
   
   }
