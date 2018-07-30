dd00cdbbec69f8121dd797bd4f70a1f7f16bc760
khalid jamal <kh.jamal95@gmail.com>
2018-07-30 18:42:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class BoardMember{
   }
   
   veto(){return "No, I must disagree"}
-  veto(){return "No, I must disagree"}
+  approve(){return "No, I must disagree"}
   veto(){return "No, I must disagree"}
   veto(){return "No, I must disagree"}
   veto(){return "No, I must disagree"}
