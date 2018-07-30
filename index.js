c6c3e37902a3719ff81e2f0e0d0026a71f5c1096
khalid jamal <kh.jamal95@gmail.com>
2018-07-30 18:31:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-class BoardMember(
-  )
+class BoardMember{
+  function(name,homeState,training){
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training;
+  }
+  
+  
+  }
